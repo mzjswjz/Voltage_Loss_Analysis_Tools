09c693ae0107b36109bf87b336cd0b15777efeb5
mzjswjz <67906305+mzjswjz@users.noreply.github.com>
2023-02-27 22:33:51 +0000

    Add some comments

--- Functions/Find_inflection_point.py
+++ Functions/Find_inflection_point.py
@@ -8,20 +8,24 @@ class Inflection_Points:
         self.precision = precision
 
     def find_inflection(self, file_id):
-        data = np.loadtxt(file_id, delimiter = ',', skiprows=1)
+
+        data = np.loadtxt(file_id, delimiter = ',', skiprows=1) #load data from file_id and read in ad float
         energy = data[:, 2]
         eqe = data[:, 3]
 
         sec_diff = np.diff(np.diff(eqe))
         new_set = np.vstack((energy[:len(sec_diff)], sec_diff))
 
-        norm_eqe = eqe / np.max(eqe)
-        plt.plot(energy, norm_eqe)
-        norm_set = new_set[1, :] / np.max(new_set[1, :])
-        plt.plot(new_set[0, :], norm_set)
-        y = np.zeros(len(energy))
-        plt.plot(energy, y, 'black', linewidth=1)
-        plt.legend(['Normalized EQE', 'Normalized numerical second derivative of EQE'], fontsize=8)
+        norm_eqe = eqe / np.max(eqe) # normalize EQE maximum value to 1
+        plt.plot(energy, norm_eqe)  #plot normalised EQE
+
+        norm_set = new_set[1, :] / np.max(new_set[1, :]) #normalize the second derivative of EQE
+        plt.plot(new_set[0, :], norm_set) #plot normalised second derivatiev of EQE
+
+        y = np.zeros(len(energy)) #creat y = 0 points at each energy value
+        plt.plot(energy, y, 'black', linewidth=1) #plot y = 0
+
+        plt.legend(['Normalized EQE', 'Normalized numerical second derivative of EQE'], fontsize=8) #define legends
         plt.xlim([1.4, 3.5])
         plt.show()
 
