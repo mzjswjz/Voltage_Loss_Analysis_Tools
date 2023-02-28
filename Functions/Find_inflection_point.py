9068491433463f982bb6062ce210d4e151761588
mzjswjz <67906305+mzjswjz@users.noreply.github.com>
2023-02-28 00:30:13 +0000

    Modify a bit

--- Functions/Find_inflection_point.py
+++ Functions/Find_inflection_point.py
@@ -1,39 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 
 
 class Inflection_Points:
 
-    def __init__(self, precision=0.0001):
-        self.precision = precision
+    def __init__(self, file_id):
+        self.file_id = file_id
+        #self.rangemin = rangemin
+        #self.rangemax = rangemax
 
-    def find_inflection(self, file_id):
+    def plot_sec_diff(self):
 
-        data = np.loadtxt(file_id, delimiter = ',', skiprows=1) #load data from file_id and read in ad float
-        energy = data[:, 2]
-        eqe = data[:, 3]
+        data = np.loadtxt(self.file_id, delimiter=',', skiprows=1)  # load data from file_id and read in ad float
+        energy = data[:, 2][::-1]
+        eqe = data[:, 3][::-1]
 
-        sec_diff = np.diff(np.diff(eqe))
-        new_set = np.vstack((energy[:len(sec_diff)], sec_diff))
+        interpolated_eqe = interp1d(energy, eqe, kind='cubic')
 
-        norm_eqe = eqe / np.max(eqe) # normalize EQE maximum value to 1
-        plt.plot(energy, norm_eqe)  #plot normalised EQE
+        # Generate new energy values for interpolation
+        new_energy = np.linspace(energy.min(), energy.max(), 300)
+        # Evaluate interpolated function at new energy values
+        new_eqe = interpolated_eqe(new_energy)
 
-        norm_set = new_set[1, :] / np.max(new_set[1, :]) #normalize the second derivative of EQE
-        plt.plot(new_set[0, :], norm_set) #plot normalised second derivatiev of EQE
+        sec_diff = np.diff(np.diff(new_eqe))
+        self.new_set = np.vstack((new_energy[:len(sec_diff)], sec_diff))
 
-        y = np.zeros(len(energy)) #creat y = 0 points at each energy value
-        plt.plot(energy, y, 'black', linewidth=1) #plot y = 0
+        norm_eqe = new_eqe / np.max(new_eqe)  # normalize EQE maximum value to 1
+        plt.plot(new_energy, norm_eqe)  # plot normalised EQE
 
-        plt.legend(['Normalized EQE', 'Normalized numerical second derivative of EQE'], fontsize=8) #define legends
-        plt.xlim([1.4, 3.5])
-        plt.show() #show the plotdsd
+        self.norm_set = self.new_set[1, :] / np.max(self.new_set[1, :])  # normalize the second derivative of EQE
+        plt.plot(self.new_set[0, :], self.norm_set)  # plot normalised second derivatiev of EQE
 
-        for n in range(len(new_set[1, :]) - 1, 2, -1):
-            a = new_set[1, n]
-            b = new_set[1, n - 1]
-            c = new_set[1, n - 2]
-            if a > 0 and b < 0 and c < 0 and abs(a - b) > self.precision:
-                inflection_pt = (energy[n] + energy[n - 1]) / 2
+        y = np.zeros(len(new_energy))  # creat y = 0 points at each energy value
+        plt.plot(new_energy, y, 'black', linewidth=1)  # plot y = 0
+
+        plt.legend(['Normalized EQE', 'Normalized numerical second derivative of EQE'], fontsize=8)  # define legends
+        plt.xlim(new_energy[0], new_energy[-1])
+        plt.show()  # show the plotd
+
+
+    def find_inflection(self,rangemin, rangemax):
+
+        indexmin = np.min(np.where(self.new_set[0,:] > rangemin))
+        indexmax = np.max(np.where(self.new_set[0, :] < rangemax))
+
+        for n in range(indexmin,indexmax, 1):
+            a = self.norm_set[n]
+            b = self.norm_set[n + 1]
+            if a > 0 and b < 0 :
+                inflection_pt = (self.new_set[0, n] + self.new_set[0, n + 1]) / 2
                 return inflection_pt
         return None
--- main.py
+++ main.py
@@ -3,17 +3,28 @@ import os
 
 def main():
     q1 = 'Where the EQE raw file is located? (including .txt)'
-    q2 = 'What is the precision you set to find inflection point? '
+ #   q2 = 'What is the precision you set to find inflection point? '
+
 
     file_id = input(q1)
-    precision = float(input(q2))
+ #   precision = float(input(q2))
+    try:
+        assert os.path.isfile(file_id), 'Invalid file path'
+        fip = FindInflect.Inflection_Points(file_id)
+        fip.plot_sec_diff()
+    except Exception as e:
+        print(f'Error: {e}')
 
-
+    q3 = 'Set a range to find inflection point (separated by comma)'
+    search_range = input(q3)
+    parts = search_range.split(",")
+    parts = [float(part) for part in parts]
+    rangemin = min(parts)
+    rangemax = max(parts)
 
     try:
         assert os.path.isfile(file_id), 'Invalid file path'
-        fip = FindInflect.Inflection_Points(precision)
-        inflection_pt = fip.find_inflection(file_id)
+        inflection_pt = fip.find_inflection(rangemin, rangemax)
         print(f'Inflection point: {inflection_pt}')
     except Exception as e:
         print(f'Error: {e}')
